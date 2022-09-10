f45d79ee8b5180aa8ce57ae8dfdc3180c5816bf9
bjd2385 <bjd2385.linux@gmail.com>
2022-09-10 13:53:51 -0400

    Retry

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -20,7 +20,7 @@ generate_tests()
         f_bats="${f_path}/${f_base_ext}.bats"
 
         if [ "$(echo "$f_base_ext" | awk '/(PASS)$/')" ]; then
-            # Forward logic
+            # PASS
             cat > "$f_bats" << EOFI
 #! /usr/bin/env bats
 
@@ -29,7 +29,7 @@ generate_tests()
 }
 EOFI
         else
-            # Backward logic
+            # FAIL
             cat > "$f_bats" << EOFI
 #! /usr/bin/env bats
 
