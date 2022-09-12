99d99f6d6b88bd18ba89f734007630aa231180e9
Brandon Doyle <brandon@sbevision.com>
2022-09-12 13:48:28 -0400

    Remove files check

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='pre-commit-gitlabci-lint',
-    version='1.1.0',
+    version='1.1.1',
     description='Validate your GitLab CI with GitLab\'s API endpoint.',
     cmdclass=cmd_classes,
     long_description=long_description,
